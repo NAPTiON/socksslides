136bca0d7008c18609c143cdfee16034d688d91e
Naption <78891049+NAPTiON@users.noreply.github.com>
2023-12-30 12:35:52 -0500

    Add Shopify theme files

---
+++ shopify-theme/SocksSlidesGPT/assets/socksgen.js
@@ -0,0 +1,8 @@
+<!-- sections/random-sock-generator.liquid -->
+
+<div id="sock-container">
+    <!-- Sock image will be displayed here -->
+</div>
+<button onclick="generateRandomSock()">Generate Random Sock</button>
+
+<script src="{{ 'your-script.js' | asset_url }}"></script>
