96ea29cde8b11f22bbeba5db4f0a3f6f946ba601
Your GitHub Username <your.email@example.com>
2023-12-31 18:08:56 -0500

    fix

--- shopify-theme/SocksSlidesGPT/snippets/custom-scripts.js
+++ shopify-theme/SocksSlidesGPT/snippets/custom-scripts.js
@@ -3,22 +3,20 @@ console.log('custom-scripts.js has been loaded');
   Custom Script for Shopify
 {%- endcomment -%}
 
-<script defer src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.1.1"></script>
-
-<script>
+<><script defer src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.1.1"></script><script>
   // Your first JavaScript code
-  console.log('Hello from the first script!');
+    console.log('Hello from the first script!');
 
-  // Your second JavaScript code
-  console.log('Hello from the second script!');
+    // Your second JavaScript code
+    console.log('Hello from the second script!');
 
-  // Additional JavaScript code using canvas-confetti
-  window.onload = function() {
-    // Trigger the confetti animation
-    confetti({
-      particleCount: 100,
-      spread: 70,
-      origin: { y: 0.6 },
-    });
-  };
-</script>
+    // Additional JavaScript code using canvas-confetti
+    window.onload = function() {
+        // Trigger the confetti animation
+        confetti({
+            particleCount: 100,
+            spread: 70,
+            origin: { y: 0.6 },
+        })};
+    };
+</script></>
