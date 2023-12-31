c5402c3f721fb4ebbaacdbf79c04910124744347
Your GitHub Username <your.email@example.com>
2023-12-31 18:03:47 -0500

    it is correct

--- shopify-theme/SocksSlidesGPT/snippets/custom-scripts.js
+++ shopify-theme/SocksSlidesGPT/snippets/custom-scripts.js
@@ -1,33 +1,24 @@
+console.log('custom-scripts.js has been loaded');
 {%- comment -%}
   Custom Script for Shopify
 {%- endcomment -%}
 
-<script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.1.1"></script>
+<script defer src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.1.1"></script>
 
-// Your first JavaScript code
-console.log('Hello from the first script!');
+<script>
+  // Your first JavaScript code
+  console.log('Hello from the first script!');
 
-// Your second JavaScript code
-console.log('Hello from the second script!');
+  // Your second JavaScript code
+  console.log('Hello from the second script!');
 
-// Additional JavaScript code using canvas-confetti
-document.addEventListener("DOMContentLoaded", function() {
-  const headerLink = document.querySelector('.header__heading-link');
-
-  if (headerLink) {
-    headerLink.addEventListener("click", function(event) {
-      // Prevent the default behavior of the link
-      event.preventDefault();
-
-      // Trigger the confetti animation
-      confetti({
-        particleCount: 100,
-        spread: 70,
-        origin: { y: 0.6 },
-      });
-
-      // You can also navigate to the link's href after the confetti animation if needed
-      // window.location.href = headerLink.getAttribute('href');
+  // Additional JavaScript code using canvas-confetti
+  window.onload = function() {
+    // Trigger the confetti animation
+    confetti({
+      particleCount: 100,
+      spread: 70,
+      origin: { y: 0.6 },
     });
-  }
-});
+  };
+</script>
